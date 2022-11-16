a5205ece80f022318b9cc94c814ab7b71985463e
Saul <phasecross@hotmail.com>
2022-11-16 11:31:22 -0700

    Added dummy home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Box,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      mode: "dark",
+      primary: {
+        main: "#e91e63",
+      },
+      secondary: {
+        main: "#8e24aa",
+      },
+      background: {
+        default: "#0a1929",
+      },
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box sx={{ height: "100%", width: "100%" }}>
+        <Typography variant="h3">Welcome to Kubergames!</Typography>
+      </Box>
+    </ThemeProvider>
   );
 }
 
