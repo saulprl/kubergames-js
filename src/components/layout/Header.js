e94a6d89d150f43c9df744601e6d5c7e45dd585c
Saul <phasecross@hotmail.com>
2022-11-17 14:19:26 -0700

    Added Header component which includes the permanent drawer and an appbar with a dynamic title

---
+++ src/components/layout/Header.js
@@ -0,0 +1,28 @@
+import { AppBar, Toolbar, Typography, useTheme } from "@mui/material";
+import PermanentDrawer from "./PermanentDrawer";
+
+const Header = (props) => {
+  const theme = useTheme();
+
+  return (
+    <>
+      <PermanentDrawer />
+      <header>
+        <AppBar
+          position="fixed"
+          sx={{
+            width: "calc(100% - 240px)",
+            ml: "240px",
+            background: theme.palette.primary.main,
+          }}
+        >
+          <Toolbar variant="dense">
+            <Typography variant="h6">{props.title}</Typography>
+          </Toolbar>
+        </AppBar>
+      </header>
+    </>
+  );
+};
+
+export default Header;
