492a9dcdb53e0b777ca2cc488d9bb51341ebe391
Saul <phasecross@hotmail.com>
2022-11-19 12:56:02 -0700

    Added a game over screen, Enter key now resets the game, snake's head is now highlighted, framerate is now 13, and changed in-game texts styling

--- src/components/games/snake-game/SnakeGame.js
+++ src/components/games/snake-game/SnakeGame.js
@@ -10,14 +10,14 @@ let paused;
 const sketch = (p5) => {
   // p5.preload = () => {
   // };
-  
+
   p5.setup = () => {
     apple = p5.loadImage("./assets/snake-game/apple.png", (img) => img);
     p5.createCanvas(500, 620);
-    snake = new Snake(p5);
+    snake = new Snake();
     score = 0;
     paused = false;
-    p5.frameRate(12);
+    p5.frameRate(13);
     pickLocation();
   };
 
@@ -30,12 +30,27 @@ const sketch = (p5) => {
     snake.update();
     snake.show();
 
+    if (snake.dead) {
+      snake.lock();
+
+      p5.fill(255);
+      p5.stroke(255, 0, 0);
+      p5.strokeWeight(6);
+      p5.textSize(40);
+      p5.textAlign(p5.CENTER, p5.CENTER);
+      p5.text("Game Over", p5.width / 2, p5.height / 2);
+
+      p5.textSize(20);
+      p5.text("Press 'Enter' to try again", p5.width / 2, p5.height / 2 + 30);
+    }
+
     if (snake.eat(food)) {
       pickLocation();
       score += 7 + p5.floor(0.2 * snake.total);
     }
 
     p5.fill(0);
+    p5.noStroke();
     p5.rect(0, 0, 500, 120);
 
     p5.noFill();
@@ -51,19 +66,28 @@ const sketch = (p5) => {
     p5.fill("#ff200d");
 
     p5.textAlign(p5.CENTER, p5.CENTER);
+    p5.fill(255);
+    p5.stroke(255, 0, 0);
+    p5.strokeWeight(4);
     p5.textSize(60);
     p5.text("" + score, 252, 63);
 
     if (p5.frameCount < 24) {
-      p5.fill("#ff200d");
-      p5.strokeWeight(1);
-      p5.textAlign(p5.CENTER, p5.CENTER);
+      p5.fill(255);
+      p5.stroke(255, 0, 0);
       p5.textSize(20);
-      p5.text("Press 'P' to pause or unpause", p5.width / 2, p5.height - 20);
+      p5.strokeWeight(3);
+      p5.textAlign(p5.CENTER, p5.CENTER);
+      p5.text("Press 'P' to pause or unpause", p5.width / 2, p5.height - 42);
+      p5.text("Press 'Enter' to reset", p5.width / 2, p5.height - 18);
     }
 
     if (paused) {
+      p5.fill(255);
+      p5.stroke(255, 0, 0);
       p5.textSize(40);
+      p5.strokeWeight(4);
+
       p5.text("Paused", p5.width / 2, p5.height / 2);
       // pause();
     }
@@ -71,21 +95,24 @@ const sketch = (p5) => {
 
   p5.keyPressed = () => {
     switch (p5.keyCode) {
+      case 13:
+        resetSketch();
+        break;
       case p5.UP_ARROW:
       case 87:
-        snake.dir(0, -1);
+        if (!snake.dead) snake.dir(0, -1);
         break;
       case p5.DOWN_ARROW:
       case 83:
-        snake.dir(0, 1);
+        if (!snake.dead) snake.dir(0, 1);
         break;
       case p5.LEFT_ARROW:
       case 65:
-        snake.dir(-1, 0);
+        if (!snake.dead) snake.dir(-1, 0);
         break;
       case p5.RIGHT_ARROW:
       case 68:
-        snake.dir(1, 0);
+        if (!snake.dead) snake.dir(1, 0);
         break;
       case 80:
         paused = !paused;
@@ -100,14 +127,21 @@ const sketch = (p5) => {
     }
   };
 
+  const resetSketch = () => {
+    snake = new Snake();
+    score = 0;
+    paused = false;
+    p5.frameRate(13);
+    p5.frameCount = 0;
+  };
+
   const pause = () => {
     p5.noLoop();
-    p5.frameRate(12);
   };
 
   const unpause = () => {
     p5.loop();
-    p5.frameRate(12);
+    p5.frameRate(13);
   };
 
   const pickLocation = () => {
@@ -136,6 +170,7 @@ const sketch = (p5) => {
       this.yspeed = 0;
       this.total = 0;
       this.tail = [];
+      this.dead = false;
 
       this.update = function () {
         if (this.total === this.tail.length) {
@@ -159,11 +194,7 @@ const sketch = (p5) => {
           this.p5.rect(this.tail[i].x, this.tail[i].y, scl, scl);
         }
 
-        this.p5.fill(255);
-        for (let i = 0; i < this.total; i++) {
-          this.p5.rect(this.tail[i].x, this.tail[i].y, scl, scl);
-        }
-
+        this.p5.fill("#e91e63");
         this.p5.rect(this.x, this.y, scl, scl);
       };
 
@@ -194,11 +225,17 @@ const sketch = (p5) => {
           let distance = this.p5.dist(this.x, this.y, pos.x, pos.y);
 
           if (distance < 1) {
-            this.total = 0;
-            this.tail = [];
-            score = 0;
+            this.dead = true;
+            // this.total = 0;
+            // this.tail = [];
+            // score = 0;
           }
         }
+      };
+
+      this.lock = function () {
+        this.xspeed = 0;
+        this.yspeed = 0;
       };
     }
   }
